75be9cbb2de7bf43964c342928e7191ce4999b09
Hugo <120035989+Hu9o73@users.noreply.github.com>
2025-05-05 14:20:31 +0200

    fix(front) Reflex placeholders
    * feat(front) Replace streamlit with reflex
    
    * bugged reflex
    
    * fix(front) Put placeholders everywhere to build reflex app properly

--- front/rxapp/pages/people.py
+++ front/rxapp/pages/people.py
@@ -1,23 +1,17 @@
 import reflex as rx
 from rxapp.components.alfred import alfred_sidebar
-from rxapp.state import AppState
+
 
 def people():
-    return rx.fragment(
-        alfred_sidebar(),
+    return rx.hstack(
         rx.box(
-            rx.heading("CRM - People"),
-            rx.hstack(
-                rx.button("Add Row", on_click=AppState.add_people_row),
-                rx.button("Export DB", on_click=AppState.export_people_df),
-                rx.button("Refresh DB", on_click=AppState.load_people_data),
-            ),
-            rx.data_table(
-                data=AppState.people_df,
-                pagination=True,
-                search=True,
-            ),
-            padding_left="320px",
-            padding="2em",
+            alfred_sidebar(),
         ),
+        rx.box(
+            rx.text("Placeholder for PEOPLE"),
+            flex="1",
+        ),
+        width="100%",
+        spacing="0",
+        align_items="flex-start"
     )
